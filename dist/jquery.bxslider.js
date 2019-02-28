52a7b4a84a1f36c491143485af5f1356148b1a35
Geiser Menoia <geiser.menoia@flaconi.de>
2019-02-28 21:00:59 +0100

    fix per https://github.com/stevenwanderski/bxslider-4/issues/1235

--- dist/jquery.bxslider.js
+++ dist/jquery.bxslider.js
@@ -368,7 +368,7 @@
           // add the current slide to the children
           children = slider.children.eq(currentIndex);
           // cycle through the remaining "showing" slides
-          for (i = 1; i <= slider.settings.maxSlides - 1; i++) {
+          for (var i = 1; i <= slider.settings.maxSlides - 1; i++) {
             // if looped back to the start
             if (currentIndex + i >= slider.children.length) {
               children = children.add(slider.children.eq(i - 1));
@@ -1106,7 +1106,11 @@
       if (e.type !== 'touchstart' && e.button !== 0) {
         return;
       }
-      e.preventDefault();
+      
+        // intencionally commented out e.preventDefault()
+      // see https://github.com/stevenwanderski/bxslider-4/pull/1214/files
+      // e.preventDefault();
+
       //disable slider controls while user is interacting with slides to avoid slider freeze that happens on touch devices when a slide swipe happens immediately after interacting with slider controls
       slider.controls.el.addClass('disabled');
 
@@ -1187,13 +1191,19 @@
 
       // x axis swipe
       if ((xMovement * 3) > yMovement && slider.settings.preventDefaultSwipeX) {
-        e.preventDefault();
+        if (e.hasOwnProperty('cancelable') && e.cancelable) {
+          e.preventDefault();
+        }
       // y axis swipe
       } else if ((yMovement * 3) > xMovement && slider.settings.preventDefaultSwipeY) {
-        e.preventDefault();
+        if (e.hasOwnProperty('cancelable') && e.cancelable) {
+          e.preventDefault();
+        }
       }
       if (e.type !== 'touchmove') {
-        e.preventDefault();
+        if (e.hasOwnProperty('cancelable') && e.cancelable) {
+          e.preventDefault();
+        }
       }
 
       if (slider.settings.mode !== 'fade' && slider.settings.oneToOneTouch) {
